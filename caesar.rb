7e38d461c58b11a1fa9d7adc632c56afb308d850
chartaseus <90235896+chartaseus@users.noreply.github.com>
2024-04-17 19:59:41 +0700

    Allow encoding uppercase letters and special characters

--- caesar.rb
+++ caesar.rb
@@ -1,14 +1,21 @@
 def caesar_cipher(string, shift)
-  alphabet = ('a'..'z').to_a
-  string_array = string.split("")
+  lowercase_alphabet = ('a'..'z').to_a
+  uppercase_alphabet = ('A'..'Z').to_a
+  string_array = string.chars
   new_array = []
   
-  string_array.each do |letter|
-    index = alphabet.index(letter)
+  string_array.each do |char|
+    index = lowercase_alphabet.index(char) || uppercase_alphabet.index(char) || 0
 
     if index + shift > 26 then index -= 26 end
-
-    new_array << alphabet[index + shift]
+    
+    if lowercase_alphabet.include?(char)
+      new_array << lowercase_alphabet[index + shift]
+    elsif uppercase_alphabet.include?(char)
+      new_array << uppercase_alphabet[index + shift]
+    else
+      new_array << char
+    end
   end
 
   return new_array.join
